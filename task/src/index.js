1f337bde18a1c9bebbecb21a6e3ff16f712eebe7
James Cat <james@catplusplus.org.uk>
2019-07-29 10:48:01 +0100

    implement redux store using provider

--- task/src/index.js
+++ task/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './reducer';
 import './index.scss';
 import App from './App';
 
 ReactDOM.render(
-  <App />, 
+  <Provider store={createStore(reducer)}>
+    <App/>
+  </Provider>, 
   document.getElementById('root')
 );
-
-
